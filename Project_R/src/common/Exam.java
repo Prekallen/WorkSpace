779f373a749f636aad532778009195ffbfcb36ac
sangcheolkim <tkdral@naver.com>
2017-06-28 11:54:27 +0900

    2017-06-28

---
+++ Project_R/src/common/Exam.java
@@ -0,0 +1,16 @@
+package common;
+
+public class Exam {
+	
+	public static void main(String[]args){
+		DBConn dbc = new DBConn();
+		dbc.a =3;
+		System.out.println(dbc.a);
+		System.out.println(DBConn.a);
+		DBConn.a = 4;
+		System.out.println(DBConn.a);
+		dbc = new DBConn();
+		System.out.println(dbc.a);
+		int a= DBConn.getInt();
+	}
+}
