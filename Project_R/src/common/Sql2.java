7302c49b9ba0162559114e9c75faa62d5e8249b0
sangcheolkim <tkdral@naver.com>
2017-06-29 16:17:08 +0900

    2017-06-29

--- Project_R/src/common/Sql2.java
+++ Project_R/src/common/Sql2.java
@@ -3,6 +3,7 @@ package common;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -31,12 +32,44 @@ public class Sql2 {
 		}
 		return null;
 	}
+	public List<HashMap> doSelect(String sql){
+		List<HashMap> userlist = new ArrayList<HashMap>();
+		try{
+			Connection con = DBConn2.getCon();
+			PreparedStatement prestmt = con.prepareStatement(sql);
+			ResultSet rs = prestmt.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			System.out.println(rsmd.getColumnName(1));
+			System.out.println(rsmd.getColumnCount());
+			while (rs.next()){
+				HashMap hm = new HashMap();
+				int colCount = rsmd.getColumnCount();
+				for(int i =1; i<=colCount;i++){
+					String colName = rsmd.getColumnName(i);
+					hm.put(colName,rs.getString(colName));
+				}
+				userlist.add(hm);
+			}
+			DBConn2.closeCon();
+			return userlist;
+		}catch(SQLException | ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	public static void main(String[]args){
 		Sql2 sql = new Sql2();
 		List<String> userList = sql.getUserIDLists();
 		for(int i=0; i<userList.size();i++){
 		System.out.println(userList.get(i));
 		}
-		
+		String order ="select ui.num, ui.name, ui.id, ui.pwd, ui.age, ci.class_name, ci.class_num from user_info as ui, class_info as ci";
+		order += " where ci.class_num=ui.class_num";
+		List<HashMap> uL = sql.doSelect(order);
+		System.out.println("-유저인포&클래스인포-");
+		for(int i=0; i<userList.size();i++){
+		System.out.println(userList.get(i));
+		}
 	}
 }
