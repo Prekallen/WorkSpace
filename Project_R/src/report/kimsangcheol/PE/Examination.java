a2a095d742d73f56c8d32b4daecbd9444dcd3bfb
sangcheolkim <tkdral@naver.com>
2017-06-30 11:16:44 +0900

    2017-06-30

--- Project_R/src/report/kimsangcheol/PE/Examination.java
+++ Project_R/src/report/kimsangcheol/PE/Examination.java
@@ -7,7 +7,7 @@ public class Examination {
 		int [] score = new int[5];
 		Scanner scan = new Scanner(System.in);
 		int temp=0;
-	
+		int sum=0;
 		void sortPrint(){
 			for(int i=0 ; i<score.length;i++){
 				for(int j=i+1;j<score.length;j++){
@@ -19,17 +19,22 @@ public class Examination {
 				}
 			}
 			for(int i=0 ; i<score.length;i++){
+				sum += score[i];
 				System.out.println(score[i]);
+				
 			}
 		}
 		
 		public static void main(String[]args){
 			Examination ex= new Examination();
 			for(int i=0;i<ex.score.length;i++){
+				
 				System.out.println((i+1) + "번째 학생의 점수를 입력하세요.>");
 				ex.score[i]=Integer.parseInt(ex.scan.nextLine());
 			
 			}
 			ex.sortPrint();
+			System.out.println("총점 : " + ex.sum);
+			System.out.println("평균 : " + (ex.sum/ex.score.length));
 		}
 }
