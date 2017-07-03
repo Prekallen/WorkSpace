387a790eb725e28a2575c80fb3709997cba44ff0
sangcheolkim <tkdral@naver.com>
2017-07-03 10:14:46 +0900

    2017-07-03

--- Project_R/src/DAO/user_info.sql
+++ Project_R/src/DAO/user_info.sql
@@ -26,3 +26,27 @@ class_num int(5) auto_increment primary key,
 class_name varchar(100) not null);
 
 select * from class_info;
+
+insert into class_info(class_name)
+values('3D프린터반');
+
+insert into user_info(id, pwd, name, age, class_num)
+values('red','red','홍길동',30, 1);
+insert into user_info(id, pwd, name, age, class_num)
+values('yellow','yellow','황길동',21, 4);
+
+select ui.num, ui.name, ui.id, ui.pwd, ui.age, ci.class_name, ci.class_num
+from user_info as ui, class_info as ci
+where ci.class_num=ui.class_num;
+
+
+alter table user_info 
+add unique index(id);
+
+select * from user_info;
+
+delete from user_info 
+where num=6;
+
+ALTER TABLE user_info
+CHANGE COLUMN num user_num int;
