32d25bcfcd8943b6b32ee79a852406acd4b9cd71
sangcheolkim <tkdral@naver.com>
2017-07-04 15:17:55 +0900

    2017-07-04

--- Project_R/src/DAO/CommentDAO.java
+++ Project_R/src/DAO/CommentDAO.java
@@ -1,5 +1,6 @@
 package DAO;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,43 +12,52 @@ import java.util.Map;
 
 import common.DBConn2;
 
-public class CommentDAO{
+public class CommentDAO {
 
 	Connection con;
-
 	CommentDAO() throws ClassNotFoundException, SQLException{
 		con = DBConn2.getCon();
 	}
 	public List<Map> getCommentList(int boardNum) throws SQLException{
-		String sql = "select ci_num, content,reg_date,user_num, board_num from comment_info where board_num=?";
+		String sql = "select ci_num, content, reg_Date, user_num, board_num from comment_info";
+		sql += " where board_num=?";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setInt(1, boardNum);
-
+		
 		ResultSet rs = ps.executeQuery();
 		ArrayList commentList = new ArrayList();
-		while (rs.next()){
+		while(rs.next()){
 			HashMap hm = new HashMap();
 			hm.put("ci_num", rs.getString("ci_num"));
 			hm.put("content", rs.getString("content"));
-			hm.put("reg_date", rs.getString("reg_date"));
+			hm.put("reg_Date", rs.getString("reg_Date"));
 			hm.put("user_num", rs.getString("user_num"));
 			commentList.add(hm);
 		}
 		rs.close();
-		rs=null;
+		rs = null;
 		ps.close();
-		ps=null;
+		ps = null;
 		return commentList;
 	}
-	public static void main(String[]args){
+	public void closeCon(){
 		try{
+			DBConn2.closeCon();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args){
+		try {
 			CommentDAO cdao = new CommentDAO();
 			List<Map> commentList = cdao.getCommentList(Integer.parseInt("1"));
 			for(Map m2 : commentList){
 				System.out.println(m2);
 			}
-		}catch(Exception e){
+			DBConn2.closeCon();
+		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
-		}
-	}	
+		} 
+		
+	}
 }
