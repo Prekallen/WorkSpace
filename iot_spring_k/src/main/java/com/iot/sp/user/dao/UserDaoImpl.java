649c8efaf64f48ab0ec30cb32a4403395fb704e1
sangcheolkim <tkdral@naver.com>
2017-08-24 18:14:01 +0900

    2017-08-24

---
+++ iot_spring_k/src/main/java/com/iot/sp/user/dao/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.iot.sp.user.dao;
+
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDaoImpl extends SqlSessionDaoSupport implements UserDao{
+
+}
